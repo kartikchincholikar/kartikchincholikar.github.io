a94fd5488a6315008e8317f36e93a436dedf2f72
Kartik Chincholikar <fallingkartik@gmail.com>
2023-06-24 12:05:59 +0530

    Update freelancer.js - removed scroll time

--- js/freelancer.js
+++ js/freelancer.js
@@ -8,9 +8,7 @@
 $(function() {
     $('.page-scroll a').bind('click', function(event) {
         var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
+        $('html, body').stop().scrollTop($($anchor.attr('href')).offset().top, 0);
         event.preventDefault();
     });
 });
